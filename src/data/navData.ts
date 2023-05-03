0e22d01c8a1d56ab525c7e09587f4f138113a903
Fouad <fouadezari@gmail.com>
2023-05-03 15:19:01 +0200

    Add Hero section and set navbar

--- src/data/navData.ts
+++ src/data/navData.ts
@@ -1,38 +1,38 @@
 type Navlink = {
-  name: string;
-  path: string;
-  dropdownLinks?: {
-    name: string;
-    path: string;
-  }[];
-};
+	name: string
+	path: string
+	dropdownLinks?: {
+		name: string
+		path: string
+	}[]
+}
 
 const navData: Navlink[] = [
-  {
-    name: "Piscines",
-    path: "/piscines/",
-    dropdownLinks: [
-      { name: "Piscine 1", path: "#!" },
-      { name: "Piscine 2", path: "#!" },
-      { name: "Piscine 3", path: "#!" },
-    ],
-  },
-  {
-    name: "Services",
-    path: "/#service/",
-  },
-  {
-    name: "Réalisation",
-    path: "/#realisation/",
-  },
-  {
-    name: "Actualités",
-    path: "/#actualites/",
-  },
-  {
-    name: "Contact",
-    path: "/contact/",
-  },
-];
+	{
+		name: "Piscines",
+		path: "/piscines/",
+		dropdownLinks: [
+			{ name: "Piscine 1", path: "/piscines/" },
+			{ name: "Piscine 2", path: "#!" },
+			{ name: "Piscine 3", path: "#!" },
+		],
+	},
+	{
+		name: "Services",
+		path: "/#service/",
+	},
+	{
+		name: "Réalisation",
+		path: "/#realisation/",
+	},
+	{
+		name: "Actualités",
+		path: "/#actualites/",
+	},
+	{
+		name: "Contact",
+		path: "/contact/",
+	},
+]
 
-export default navData;
+export default navData
