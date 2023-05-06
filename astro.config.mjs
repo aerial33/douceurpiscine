7ab9f26a36a1e9e2930c2c81139a955e7a558ccb
Fouad <fouadezari@gmail.com>
2023-05-06 18:50:05 +0200

    feat: add to page's pool card and gricard

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,9 @@
 import { defineConfig } from 'astro/config'
 import mdx from '@astrojs/mdx'
 import tailwind from '@astrojs/tailwind'
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
-	integrations: [mdx(), tailwind({ config: { applyBaseStyles: false } })],
+	integrations: [mdx(), tailwind({ config: { applyBaseStyles: false } }), preact()],
 })
