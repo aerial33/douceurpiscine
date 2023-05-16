9193c6ee429c8b1fc8f4aeb57b74d8e4e79f51e3
Fouad <fouadezari@gmail.com>
2023-05-16 17:07:33 +0200

    feat: add animation in loading with intersection-observer

---
+++ src/js/main.js
@@ -0,0 +1,66 @@
+const navElement = () => {
+	document.addEventListener("DOMContentLoaded", () => {
+		const navBtn = document.getElementById("menu")
+		const menuBar = document.querySelector('[role="menubar"]')
+		const header = document.querySelector("header")
+		const dropdownBtn = document.getElementById("dropdown-btn")
+		const dropdown = document.querySelector('[role="listbox"]')
+		const navLinks = document.querySelectorAll("[data-navLink]")
+
+		window.addEventListener("scroll", () => {
+			const isScrolled = window.pageYOffset > 20
+			header.classList.toggle("backdrop-blur-lg", isScrolled)
+		})
+
+		navBtn.addEventListener("click", () => {
+			menuBar.classList.toggle("hidden")
+		})
+		dropdownBtn.addEventListener("click", e => {
+			// dropdown.classList.toggle("hidden")
+			e.preventDefault()
+			dropdown.classList.toggle("hidden")
+			e.stopPropagation()
+		})
+
+		document.addEventListener("click", () => {
+			dropdown.classList.add("hidden")
+		})
+
+		navLinks.forEach(link => {
+			if (link.getAttribute("href") === window.location.pathname) {
+				link.setAttribute("aria-current", "page")
+			}
+		})
+	})
+}
+
+const animateLoading = () =>{
+	const sliders = document.querySelectorAll('.slide-in')
+	const faders = document.querySelectorAll(".fade-in")
+
+	const appearOptions = {
+		threshold: 0,
+		rootMargin: "0px 0px 0px 0px"
+	}
+
+	const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+		entries.forEach(entry => {
+			if (!entry.isIntersecting){
+				return
+			} else {
+				entry.target.classList.add('appear')
+				appearOnScroll.unobserve(entry.target)
+			}
+		})
+	}, appearOptions)
+
+	faders.forEach(fade => {
+		appearOnScroll.observe(fade)
+	})
+
+	sliders.forEach(slide => {
+		appearOnScroll.observe(slide)
+	})
+}
+
+export { navElement, animateLoading }
